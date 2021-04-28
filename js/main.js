ec1f2ae16e8f4674cf8678fa49ee4a2c1a387772
jhonnymosquera <jhonnymosquera.web@gmail.com>
2021-04-27 21:52:07 -0500

    optimization js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ const btnBlack = document.querySelector('.btn-black');
 const btnRed = document.querySelector('.btn-red');
 const btnWhite = document.querySelector('.btn-white');
 
-function cambiarCarro(carColor) {
+function changeCar(carColor) {
 	car.setAttribute('src', `/img/veiculos/car-${carColor}.png`);
 	car.setAttribute('alt', `car ${carColor}`);
 }
 
-btnBlue.addEventListener('click', () => cambiarCarro('blue'));
-btnBlack.addEventListener('click', () => cambiarCarro('black'));
-btnRed.addEventListener('click', () => cambiarCarro('red'));
-btnWhite.addEventListener('click', () => cambiarCarro('white'));
+function addEventClick(btn, color) {
+	btn.addEventListener('click', () => changeCar(color));
+}
+
+addEventClick(btnBlue, 'blue');
+addEventClick(btnBlack, 'black');
+addEventClick(btnRed, 'red');
+addEventClick(btnWhite, 'white');
