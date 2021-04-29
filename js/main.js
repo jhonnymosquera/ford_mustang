b6e7deea69f1cb7dd9b9b1ee3574523550c0e13f
jhonnymosquera <jhonnymosquera.web@gmail.com>
2021-04-28 20:12:36 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 class BtnChangeColorCar {
 	constructor(color) {
-		this.color = color;
 		this.car = document.querySelector('#car');
 		this.btn = document.querySelector(`.btn-${color}`);
 		this.btn.addEventListener('click', () => {
-			car.setAttribute('src', `/img/veiculos/car-${this.color}.png`);
-			car.setAttribute('alt', `car ${this.color}`);
+			car.setAttribute('src', `img/veiculos/car-${color}.png`);
+			car.setAttribute('alt', `car ${color}`);
 		});
 	}
 }
